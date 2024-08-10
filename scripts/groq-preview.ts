6aabbf368dbc7d60bb5a31c1455de636ba0f35ee
Tristan Chin <tristan.chin@chintristan.io>
2024-08-10 00:00:20 -0400

    Add groq preview script

---
+++ scripts/groq-preview.ts
@@ -0,0 +1,27 @@
+import { postDetailsSelection } from "@/sanity/selections/post-details";
+import { existsSync } from "fs";
+import fs from "fs/promises";
+import { q } from "groqd";
+import { format } from "groqfmt-nodejs";
+import path from "path";
+
+(async () => {
+  try {
+    const queryFile = path.join(__dirname, "sandbox.groq");
+    if (!existsSync(queryFile)) {
+      await fs.writeFile(
+        queryFile,
+        `*[_type == "post"]{ title, slug }`,
+        "utf-8"
+      );
+      console.info("sandbox.groq created");
+    }
+
+    const query = q("*").filter("_type == 'post'").grab$(postDetailsSelection);
+
+    const queryFormatted = format(query.query);
+    await fs.writeFile(queryFile, queryFormatted, "utf-8");
+  } catch (error) {
+    console.error(error);
+  }
+})();
